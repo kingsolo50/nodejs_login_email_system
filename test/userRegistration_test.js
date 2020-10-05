ff3e01af68bbbc0b40c3c7975ba1db193e035fbc
Solomon Kamanu <skk84@hotmail.com>
2020-10-06 00:33:26 +0100

    User save test successfull

--- test/userRegistration_test.js
+++ test/userRegistration_test.js
@@ -3,7 +3,40 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const localUser = require('../models/localUser');
 
-describe('User registration', function() {
+
+  describe('User', function () {
+    describe('#save()', function () {
+      it('should save without error', function () {
+        var user = new localUser({
+          email: 'email@hotmail.com',
+          password: 'password',
+          username: 'username', 
+          pin: 1234,       
+          firstName: 'solo',
+          lastName: 'mon',
+          postCode: 'e12 6aq',
+          contactNumber: '07949849131'
+        });
+        user.save(function (done, err) {
+          if (err) done(err);
+          else done();
+        });
+      });
+    });
+
+    //2
+    it('returns a user object')
+
+    //3
+    it('creates hash password')
+
+    //4
+    it('throws an error when the user already exist')
+
+    //5
+    it('send newly registered user validation email')
+
+  });
   
   // const user = new localUser({
   //   password: 'password',
@@ -15,20 +48,7 @@ describe('User registration', function() {
   //   contactNumber: '07949849131'
   // });
 
-  //1
-  it('adds user to database');
+ 
 
-  //2
-  it('returns a user object')
-
-  //3
-  it('creates hash password')
-
-  //4
-  it('throws an error when the user already exist')
-
-  //5
-  it('send newly registered user validation email')
-
-  });
+  
 
